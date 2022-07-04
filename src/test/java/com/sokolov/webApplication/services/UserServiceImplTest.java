78c51c6bbb38de551e8c328754a6abc5bb5a8003
Sokolov <waumok@gmail.com>
2022-07-04 11:53:00 +0300

    version 0.1.5 common refactor and fix

--- src/test/java/com/sokolov/webApplication/services/UserServiceImplTest.java
+++ src/test/java/com/sokolov/webApplication/services/UserServiceImplTest.java
@@ -4,21 +4,14 @@ import com.sokolov.webApplication.exeption.NotFoundEntityException;
 import com.sokolov.webApplication.models.Role;
 import com.sokolov.webApplication.models.User;
 import com.sokolov.webApplication.repository.Repository;
-import com.sokolov.webApplication.repository.RepositoryImpl;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
